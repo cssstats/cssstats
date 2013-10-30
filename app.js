22a2335a26f45c755df7dc478eeb6d8cd90358c2
mrmrs <hi@mrmrs.cc>
2013-10-30 03:40:57 -0700

    Adds dimensions

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ $(function(){
   var link_styles = [];
 
   var appWidth = $("svg").parent().width();
+
   var appHeight = $("svg").parent().height();
 
   d3.json("myspace.json", function(error, json) { // Load Json, pass it to d3.
@@ -29,6 +30,10 @@ $(function(){
         cssvalues.push(rules[i].declarations[0].value);
         cssproperties.push(rules[i].declarations[0].property);
       }
+      if(rules[i].declarations && rules[i].declarations[0].property === "width") {
+        element_width_declarations.push(rules[i].declarations[0].value);
+        element_width_declarations.push(rules[i].declarations[0].property);
+      }
       if(rules[i].declarations && rules[i].declarations[0].property === "background-color") {
         bg_color_values.push(rules[i].declarations[0].value);
       }
@@ -36,6 +41,8 @@ $(function(){
         font_sizes.push(rules[i].declarations[0].value);
       }
       cssvalues.sort();
+      element_width_declarations.sort();
+      var element_widths_unique = element_width_declarations.sort().filter(onlyUnique);
       var cssvalues_unique = cssvalues.sort().filter(onlyUnique);
       var bg_colors_unique = bg_color_values.sort().filter(onlyUnique);
       cssproperties.sort();
@@ -44,11 +51,11 @@ $(function(){
 
     }
 
-    var svgContainer = d3.select("body").append("div.colors")
+    var svgContainer = d3.select("body").append("div").attr("class", "colors")
       .attr("width", "100%")
       .attr("height", "100px");
 
-    var textContainer = d3.select("body").append("div.fonts")
+    var textContainer = d3.select("body").append("div").attr("class", "fonts")
       .attr("width", "100%")
       .attr("height", "75px");
 
@@ -75,11 +82,21 @@ $(function(){
       .data(fontsizes_unique)
       .enter()
       .append("div")
-      .text(function(d) { return d; })
+      .text(function(d) { return d + " The quick brown fox jumps over the lazy dog"; })
       .style("font-size", function(d) {return d + " ";})
       .style("text-align", "center")
       .style("padding", "40px 0")
 
+    var dimensions = textContainer.selectAll("div.dimensions")
+      .data(element_widths_unique)
+      .enter()
+      .append("div")
+      .text(function(d) { return d; })
+      .style("width", function(d) {return d + " ";})
+      .style("outline", "1px solid #333")
+      .style("margin-bottom", "20px")
+      .style("line-height", "8")
+      .style("text-align", "center")
 
     console.log(cssvalues);
     console.log(cssvalues + cssproperties);
