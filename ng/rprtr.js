075fe7a80873ea8be84a625bb0723dc4ecfb4b0a
Brent Jackson <jxnblk@gmail.com>
2013-11-02 13:18:59 -0400

    Adding declarations view

--- ng/rprtr.js
+++ ng/rprtr.js
@@ -5,20 +5,20 @@
 var rprtr = angular.module('rprtr',[])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/declarations', {templateUrl: 'partials/declarations.html', controller: 'DeclarationsCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
 
 
 rprtr.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $scope.herro = 'herro world';
+  $scope.loading = true;
 
   // Setting as a scope variable that can be updated in the view
   $scope.styleUrl = 'data/myspace.json';
 
   // Function to get the styles data
   $scope.getStyles = function(styleUrl) {
-    $scope.loading = true;
     $http.get(styleUrl).success(function(res) {
       $scope.styles = res;
       $scope.loading = false;
@@ -28,5 +28,18 @@ rprtr.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
   // Getting initial styles data
   // This function can later be called from the view, if needed.
   $scope.getStyles($scope.styleUrl);
+
+}]);
+
+rprtr.controller('DeclarationsCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.loading = true;
+
+  $scope.getDeclarations = function(styleUrl) {
+    $http.get(styleUrl).success(function(res) {
+      //$scope.styles = res;
+      //$scope.loading = false;
+    });
+  };
 
 }]);
