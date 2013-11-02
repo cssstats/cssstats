b77ccf2d1622b6aeb20ca0e7a2cb6ec704a84045
Brent Jackson <jxnblk@gmail.com>
2013-11-02 14:21:40 -0400

    List all declarations

--- ng/rprtr.js
+++ ng/rprtr.js
@@ -18,6 +18,7 @@ rprtr.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.styleUrl = 'data/myspace.json';
 
   // Function to get the styles data
+  // This should really go in a factory
   $scope.getStyles = function(styleUrl) {
     $http.get(styleUrl).success(function(res) {
       $scope.styles = res;
@@ -34,12 +35,28 @@ rprtr.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 rprtr.controller('DeclarationsCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.loading = true;
+  $scope.styleUrl = 'data/myspace.json';
 
+  // This belongs in a factory
   $scope.getDeclarations = function(styleUrl) {
+    $scope.loading = true;
     $http.get(styleUrl).success(function(res) {
+      var rules = res.stylesheet.rules;
+      //console.log(rules.length);
+      $scope.declarations = [];
+      for(var i = 0; i < rules.length; i++){
+        var declarations = rules[i].declarations;
+        for(var j in declarations){
+          //console.log(declarations[j].property + ': ' + declarations[j].value);
+          $scope.declarations.push(declarations[j].property + ': ' + declarations[j].value);
+        };
+      };
+      $scope.loading = false;
       //$scope.styles = res;
       //$scope.loading = false;
     });
   };
+
+  $scope.getDeclarations($scope.styleUrl);
 
 }]);
