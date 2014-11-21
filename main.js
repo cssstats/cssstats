864949659cda93a0b4f1de65cb13dbc66701de59
Brent Jackson <jxnblk@github.com>
2014-11-21 13:41:44 -0500

    Adjust static assets path

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 var secret = Math.random().toString(36).slice(2);
 app.use(session({ secret: secret }));
