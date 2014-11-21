b748fddbb25f3e6942cf91a39e8adea16bf8fe35
Brent Jackson <jxnblk@github.com>
2014-11-21 10:42:03 -0500

    Clean up CSS and use Uncss

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ var gulp = require('gulp');
 var basswork = require('gulp-basswork');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var uncss = require('gulp-uncss');
+var glob = require('glob');
 
 gulp.task('css', function() {
   gulp.src('./css/base.css')
     .pipe(basswork())
+    .pipe(uncss({
+      html: glob.sync('./views/**/*.handlebars'),
+      ignore: []
+    }))
     .pipe(minifyCss())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./public/css'));
