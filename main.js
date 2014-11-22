5b5e1e96b9228ae1643794462ae8619c2593e146
Brent Jackson <jacksonblack@gmail.com>
2014-11-22 11:10:01 -0500

    Remove old ignores

--- main.js
+++ main.js
@@ -76,6 +76,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  console.error(err.stack);
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
