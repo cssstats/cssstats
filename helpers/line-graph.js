6f28f29656de1aa7d2eb53156bbb9ca99fec336a
Brent Jackson <jacksonblack@gmail.com>
2014-11-26 11:22:32 -0500

    Use chartable modules for graphs and charts

--- helpers/line-graph.js
+++ helpers/line-graph.js
@@ -1,51 +1,14 @@
 
-var fs = require('fs');
-var path = require('path');
 var Handlebars = require('handlebars');
+var chartable = require('chartable');
 
 module.exports = function(array, width, height) {
 
   if (!Array.isArray(array)) return 'Not an Array';
 
-  var template = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'line-graph.handlebars'), 'utf8'));
+  var html = chartable.lineGraph(array, { width: width, height: height });
 
-  var svg;
-  var xStep = 1;
-  var yStep;
-  var ymax = 1;
-  var model = {};
-
-  model.d = '';
-  model.width = width || 1024;
-  model.height = height || 512;
-  model.rules = [];
-
-  xStep = width / array.length;
-
-  array.map(function(val) {
-    if (val > ymax) {
-      ymax = val;
-    }
-  });
-
-  model.d = 'M0 ' + height + ' ';
-  array.forEach(function(y, i) {
-    model.d += 'L ' + (xStep * i) + ' ' + (model.height - ((model.height/ymax) * y)) + ' ';
-  });
-  model.d += 'L ' + model.width + ' ' + model.height + 'z';
-
-  yStep = model.height / 4;
-  for (var i = 0; i < 4; i++) {
-    model.rules.push({
-      y: yStep * i,
-      textY: yStep * i + 16,
-      value: ymax * (1 - (.25 * i))
-    });
-  }
-
-  svg = template(model);
-
-  return new Handlebars.SafeString(svg);
+  return new Handlebars.SafeString(html);
 
 };
 
--- helpers/uniques-graph.js
+++ helpers/uniques-graph.js
@@ -1,43 +1,27 @@
 
 var Handlebars = require('handlebars');
+var chartable = require('chartable');
 
 module.exports = function(obj, max) {
 
   if (!obj) console.error('no uniques object!');
 
-  var svg;
-  var width = 256;
-  var height = 240;
-  var padY = 20;
+  var html;
 
-  svg = '<svg viewBox="0 0 ' + width + ' ' + (height + 2 * padY) + '" fill="currentcolor" font-weight="bold">\n' +
-        '  <defs>\n' +
-        '    <pattern id="hash" x="0" y="0" width="8" height="8"' +
-          ' patternUnits="userSpaceOnUse" patternTransform="rotate(45), scale(2)">\n' +
-        '      <rect width="8" height="8" fill="white"/>\n' +
-        '      <path d="M0 1 H16 M0 5 H16 M0 9 H16 M0 13 H16" fill="none" stroke="currentcolor"/>\n' +
-        '    </pattern>\n' +
-        '  </defs>\n' +
-        '  <g>\n' +
-        '    <path d="M0 ' + padY + ' H' + width + '" fill="none" stroke="currentcolor" opacity="0.5"/>\n' +
-        '    <path d="M0 ' + (0.25 * height + padY) + ' H' + width + '" fill="none" stroke="currentcolor" opacity="0.5"/>\n' +
-        '    <path d="M0 ' + (0.5 * height + padY) + ' H' + width + '" fill="none" stroke="currentcolor" opacity="0.5"/>\n' +
-        '    <path d="M0 ' + (0.75 * height + padY) + ' H' + width + '" fill="none" stroke="currentcolor" opacity="0.5"/>\n' +
-        '    <path d="M0 ' + (height + padY) + ' H' + width + '" fill="none" stroke="currentcolor" opacity="0.5"/>\n' +
-        '  </g>\n' +
-        '  <rect x="20" y="' + (height - (obj.percentTotal * height) + padY) +
-          '" width="96" height="' + (obj.percentTotal * height) + '"/>\n' +
-        '  <text x="66" y="' + (height - (obj.percentTotal * height) + padY - 6) +
-          '" text-anchor="middle">' + obj.total + '</text>\n' +
-        '  <rect x="140" y="' + (height - (obj.percentUnique * height) + padY) +
-          '" width="96" height="' + (obj.percentUnique * height) + '" fill="url(#hash)" opacity="0.75" stroke="currentcolor" stroke-width="2" />\n' +
-        '  <text x="190" y="' + (height - (obj.percentUnique * height) + padY - 6) +
-          '" text-anchor="middle">' + obj.unique + '</text>\n' +
-        '  <text x="66" y="' + (height + 2 * padY - 3) + '" text-anchor="middle">Total</text>\n' +
-        '  <text x="190" y="' + (height + 2 * padY - 3) + '" text-anchor="middle">Unique</text>\n' +
-        '</svg>';
+  var data = [obj.total, obj.unique];
 
-  return new Handlebars.SafeString(svg);
+  var options = {
+    width: 384,
+    height: 240,
+    yMax: max,
+    yPadding: 20,
+    ruleLabels: false,
+    labels: ['Total', 'Unique']
+  };
+
+  html = chartable.barChart(data, options);
+
+  return new Handlebars.SafeString(html);
 
 };
 
