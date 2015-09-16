eda51690fb8e46bfe3a7bc982fc253bcfeb03df9
John Otander <johnotander@gmail.com>
2015-09-16 11:11:11 -0600

    Validate url presence and format

--- routes/parse.js
+++ routes/parse.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var formidable = require('formidable');
 var normalizeUrl = require('normalize-url');
+var isPresent = require('is-present');
+var isUrl = require('is-url');
 var isCss = require('is-css');
 
 var router = express.Router();
@@ -13,12 +15,17 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   var form = new formidable.IncomingForm();
   form.parse(req, function(error, fields, files) {
-    var url = normalizeUrl(fields.url);
 
-    if (isCss(url)) {
-      res.redirect('/stats?link=' + encodeURIComponent(url));
+    if (isPresent(fields.url) && isUrl(fields.url)) {
+      var url = normalizeUrl(fields.url || '');
+
+      if (isCss(url)) {
+        res.redirect('/stats?link=' + encodeURIComponent(url));
+      } else {
+        res.redirect('/stats?url=' + encodeURIComponent(url));
+      }
     } else {
-      res.redirect('/stats?url=' + encodeURIComponent(url));
+      res.render('index', { error: 'Please provide a valid url' });
     }
   });
 });
