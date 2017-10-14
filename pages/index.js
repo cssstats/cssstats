e9c22f8c93c09806a0ad8474d8f98af243138905
John Otander <johnotander@gmail.com>
2017-10-14 12:45:51 -0600

    Correctly use ThemeProvider

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
+import withTheme from '../components/withTheme'
 import Layout from '../components/Layout'
+import Button from '../components/Button'
+import Input from '../components/Input'
 
-export default () =>
-  <Layout children='Yay!' />
+const Index = () =>
+  <Layout>
+    <Input />
+    <Button children='Button' />
+  </Layout>
+
+export default withTheme(Index)
