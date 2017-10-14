4dc88c04498722caae83f1a428476933ae83f353
John Otander <johnotander@gmail.com>
2017-10-14 11:11:42 -0600

    Implement basic ssr

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+const next = require('next')
 const Router = require('router')
 const finalhandler = require('finalhandler')
+const { parse } = require('url')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 const router = Router()
 
-router.get('/api/stats', require('./lib/stats'))
-router.get('/stats', require('./lib/app'))
-router.get('/', require('./lib/app'))
+app
+  .prepare()
+  .then(() => {
+    router.get('/api/stats', require('./lib/stats'))
 
-module.exports = (req, res) => router(req, res, finalhandler(req, res))
+    router.get('*', (req, res) => {
+      handle(req, res, parse(req.url, true))
+    })
+
+    router.get('/', (req, res) => {
+      app.render(req, res, '/', req.query)
+    })
+  })
+
+module.exports = (req, res) => {
+  router(req, res, finalhandler(req, res))
+}
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import Layout from '../components/Layout'
+
+export default () =>
+  <Layout children='Yay!' />
