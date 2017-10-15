86896a4bd5305874b4abade396219215841bd49a
John Otander <johnotander@gmail.com>
2017-10-15 09:46:32 -0600

    Add color components and code block

---
+++ app/Text.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import { space, fontSize, width, color, fontWeight } from 'styled-system'
+
+const Text = styled('div')(
+  [],
+  props => ({}),
+  space,
+  fontSize,
+  width,
+  color,
+  fontWeight
+)
+
+Text.defaultProps = {
+  fontWeight: 'normal'
+}
+
+export default Text
