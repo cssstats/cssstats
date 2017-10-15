228777a9dd71e61ed1fa2bf40972d0c08f1a207b
John Otander <johnotander@gmail.com>
2017-10-14 20:02:05 -0600

    Begin reorganizing to use babel directly

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const next = require('next')
 const Router = require('router')
 const finalhandler = require('finalhandler')
-const { parse } = require('url')
-
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
 
 const router = Router()
 
-app
-  .prepare()
-  .then(() => {
-    router.get('/api/stats', require('./api/stats'))
-
-    router.get('*', (req, res) => {
-      handle(req, res, parse(req.url, true))
-    })
-  })
+router.get('/api/stats', require('./api/stats'))
+router.get('*', require('./app')
 
 module.exports = (req, res) => {
   router(req, res, finalhandler(req, res))
