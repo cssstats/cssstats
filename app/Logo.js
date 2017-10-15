6d1e8e947e8bd63be7b9f9debe12df04bacebc3f
John Otander <johnotander@gmail.com>
2017-10-15 10:36:42 -0600

    Add logo

---
+++ app/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Div from './Div'
+import Flex from './Flex'
+import Text from './Text'
+
+const Logo = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    width={props.size}
+    height={props.size}
+    fill={props.color || 'currentcolor'}
+  >
+    <g fontFamily="inherit" fontSize="12px" fontWeight="bold">
+      <text x="0.5" y="11" children="{" />
+      <text x="15.5" y="11" textAnchor="end" children="}" />
+    </g>
+
+    <rect x="5" y="7" width="2.5" height="4" />
+    <rect x="8.5" y="5" width="2.5" height="6" />
+  </svg>
+)
+
+export default Logo
