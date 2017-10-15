b5881faf892bd0830ba06644a8aa464482f424d1
John Otander <johnotander@gmail.com>
2017-10-15 11:22:40 -0600

    Run x0 and micro-dev together

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const next = require('next')
 const Router = require('router')
 const finalhandler = require('finalhandler')
 
 const router = Router()
 
 router.get('/api/stats', require('./api/stats'))
-router.get('*', require('./app')
 
 module.exports = (req, res) => {
   router(req, res, finalhandler(req, res))
