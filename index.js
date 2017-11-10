fe6dca03ff036469cde4fd64ca8807c9d5376797
John Otander <johnotander@gmail.com>
2017-11-10 10:23:18 -0700

    Add file support

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Router = require('router')
+const send = require('send')
 const finalhandler = require('finalhandler')
 
 const router = Router()
 
 router.get('/api/stats', require('./api/stats'))
+
+router.get('/', (req, res) => send(req, 'public/index.html').pipe(res))
+router.get('/bundle.js', (req, res) => send(req, 'public/bundle.js').pipe(res))
 
 module.exports = (req, res) => {
   router(req, res, finalhandler(req, res))
