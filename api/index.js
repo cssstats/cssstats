eb44d4fb1ca0cb00221806f90805acfebf8ce46c
John Otander <johnotander@gmail.com>
2018-03-28 07:42:23 -0600

    Add cors support

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const Router = require("router")
 const finalhandler = require("finalhandler")
+const cors = require('micro-cors')()
 
 const router = Router()
 
-router.get("/stats", require("./stats"))
+router.get("/stats", cors(require("./stats")))
 
 module.exports = (req, res) => {
   router(req, res, finalhandler(req, res))
