a1defeeacf9e59646862de67f07980c361ec292e
John Otander <johnotander@gmail.com>
2019-07-26 10:08:07 -0600

    Add css endpoint

---
+++ api/css/index.js
@@ -0,0 +1,38 @@
+const { send } = require('micro')
+const cors = require('micro-cors')()
+const getCss = require('get-css')
+const getParam = require('get-query-param')
+const isUrl = require('is-url')
+const isPresent = require('is-present')
+const normalizeUrl = require('normalize-url')
+const cssbeautify = require('cssbeautify')
+
+const isValidUrl = url => isPresent(url) && isUrl(url)
+
+const cssApi = async (req, res) => {
+  const url = getParam('url', req.url)
+
+  const fullUrl = url && normalizeUrl(url)
+
+  if (!isValidUrl(fullUrl)) {
+    return send(res, 406, {
+      error: 'unacceptable',
+      message: 'Url is invalid'
+    })
+  }
+
+  try {
+    const css = await getCss(fullUrl)
+    css.css = cssbeautify(css.css)
+
+    send(res, 200, { css })
+  } catch (e) {
+    send(res, 500, {
+      error: 'server_error',
+      message: 'Something went wrong',
+      stack: e
+    })
+  }
+}
+
+module.exports = cors(cssApi)
