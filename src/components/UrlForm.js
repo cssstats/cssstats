40855d3a61a33a77ac970ac21b6d57208fa1328d
Vince Speelman <v@vinspee.me>
2019-07-26 20:44:10 -0400

    add input type="url"
    `inputMode` has very little browser support (https://caniuse.com/#search=inputmode), but `type="url"` has really good support (https://caniuse.com/#search=type%3Durl). This adds both

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -22,6 +22,7 @@ export default ({ showLabel = false }) => {
         placeholder="google.com"
         inputMode="url"
         value={url}
+        type="url"
         onChange={e => setUrl(e.target.value)}
         sx={{ color: 'text' }}
       />
